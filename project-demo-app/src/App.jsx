71d2f9a0c74aa47743f1fb5196064ca67540b904
Sahan-N <sahann@gmail.com>
2025-11-15 12:03:26 +0530

    adding a card

--- project-demo-app/src/App.jsx
+++ project-demo-app/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header"
 import Footer from "./footer";
+import Card from "./card";
 
 function App() {
   return(
     <>
     <Header/>
+    <Card/>
     <Footer/>
     </>
 
---
+++ project-demo-app/src/Card.jsx
@@ -0,0 +1,15 @@
+import profilePic from './assets/profile.jpg'
+function Card(){
+
+    return(
+        <div className="card">
+            <img src="profilePic" alt="profile picture"></img>
+            <h2>Sahan</h2>
+            <p>I make software and play games  </p>
+
+        </div>
+    );
+
+}
+
+export default Card
