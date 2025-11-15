f63513b8405f4ec3c79d2ce3ef9c976ea088b9c1
Sahan-N <sahann@gmail.com>
2025-11-15 12:51:55 +0530

    add a card with css styles

--- project-demo-app/src/Card.jsx
+++ project-demo-app/src/Card.jsx
@@ -3,9 +3,9 @@ function Card(){
 
     return(
         <div className="card">
-            <img src="profilePic" alt="profile picture"></img>
-            <h2>Sahan</h2>
-            <p>I make software and play games  </p>
+            <img className='card-image' src={profilePic} alt="profile picture"></img>
+            <h2 className='card-name'>Sahan</h2>
+            <p className='card-description'>I make software and play games  </p>
 
         </div>
     );
